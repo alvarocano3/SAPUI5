85f2fb5de70171d72aa592c85741959872e15418
alvarocano3 <alvarocano@entrepinares.es>
2021-03-30 08:44:30 +0000

    My First Commit

---
+++ MyFirstProject/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.EntrepinaresUI5.MyFirstProject.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
